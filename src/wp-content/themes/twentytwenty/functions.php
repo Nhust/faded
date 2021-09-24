2f667e3615cc233d673893eeb9776a2cd333573f
nhust <worfut@hotmail.fr>
2021-09-25 01:04:32 +0200

    Nav is active, fix css nav

--- src/wp-content/themes/twentytwenty/functions.php
+++ src/wp-content/themes/twentytwenty/functions.php
@@ -1278,3 +1278,12 @@ function getListKeyBind($params)
     }
 
 }
+
+
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+function special_nav_class($classes, $item){
+    if( in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
