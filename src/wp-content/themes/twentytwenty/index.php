4612b93117d79bd32bf842b0b079f46f04432081
nhust <nhust@MacBook-Pro-de-Pierre-2.local>
2021-10-04 00:01:38 +0200

    Try to remove something

--- src/wp-content/themes/twentytwenty/index.php
+++ src/wp-content/themes/twentytwenty/index.php
@@ -47,7 +47,6 @@ get_header();
 
                             if (have_posts()) {
 
-                                $i = 0;
 
                                 while (have_posts()) {
                                     the_post();
